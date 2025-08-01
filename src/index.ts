fa6bd9e9072cb14ce7ec62bc4e66d34b02c32aa7
GibsonCS <gibson.cruzds@gmail.com>
2025-07-31 23:23:29 -0300

    feat(strategy): use strategy patterns

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,42 @@
-// import PostgresRepository from "./repositories/postgresRepository.ts";
+import PostgresRepository from "./repositories/postgresRepository.ts";
 import MongoRepository from "./repositories/mongoRepository.ts";
-import { type Product } from "./types/product.ts";
+import DatabaseContext from "./strategy/databaseContext.ts";
 
 const MONGO_URL_CONNECTION = process.env.MONGO_CONNECTION_STRING || "";
+const POSTGRES_URL_CONNECTION = process.env.POSTGRES_CONNECTION_STRING || "";
 
-const mongo = new MongoRepository(MONGO_URL_CONNECTION);
+const postgresContext = new DatabaseContext(
+  new PostgresRepository(POSTGRES_URL_CONNECTION)
+);
+const mongodbContext = new DatabaseContext(
+  new MongoRepository(MONGO_URL_CONNECTION)
+);
 
-const product: Product = {
-  name: "Sabao",
-  category: "limpeza",
-  price: 6.56,
+const data = [
+  {
+    name: "Sabao",
+    category: "limpeza",
+    price: 6.56,
+    type: "logger",
+  },
+  {
+    name: "Gibson",
+    username: "gibson.cruz",
+    password: "1111",
+    type: "register",
+  },
+];
+
+const contextType = {
+  logger: mongodbContext,
+  register: postgresContext,
 };
+
+for (const { type, ...item } of data) {
+  const context = contextType[type];
+  await context.connect();
+  await context.save(item);
+
+  console.log(type, context.dbContext.constructor.name);
+  console.log(await context.findAll());
+}
