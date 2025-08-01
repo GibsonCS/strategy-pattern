05b210d0ab2f6d2721ff90f625645af787f0b7a5
GibsonCS <gibson.cruzds@gmail.com>
2025-07-31 21:15:53 -0300

    feat(mongo): insert mongo script into script.sh

--- script.sh
+++ script.sh
@@ -6,3 +6,11 @@ docker run \
     -p 5432:5432 \
     -d \
     postgres
+
+docker run \
+    --name mongodb \
+    -e MONGO_INITDB_ROOT_USERNAME=gibson \
+    -e MONGO_INITDB_ROOT_PASSWORD="senha" \
+    -p 27017:27017 \
+    -d \
+    mongo:8
